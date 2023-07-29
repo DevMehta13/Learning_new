05b8c13907e83e59f8316859c5d1af2e9f2e3ca1
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-07-29 12:46:02 +0530

    Add files via upload

---
+++ # 16 - armstrongNumber.c
@@ -0,0 +1,23 @@
+// Program: To check if entered number is armstrong number or not.
+// Author: Dev Mehta
+// Date: 29th July 2023.
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int number,originalNumber,sum=0;
+    printf("Enter the number: ");
+    scanf("%d" , &number);
+    originalNumber=number;
+    while(number!=0){
+        int random = number % 10;
+        int random2 = pow(random,3);
+        sum += random2;
+        number/=10;
+    }
+    if(sum=originalNumber){
+        printf("%d is an armstrong number." , originalNumber);
+    }
+    else{
+        printf("%d is not an armstrong number." , originalNumber);
+    }
+}
