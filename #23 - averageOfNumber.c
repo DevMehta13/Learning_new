141afbfaa0e97ae492b30071365697de206618f7
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-07-29 17:12:16 +0530

    Add files via upload

---
+++ #23 - averageOfNumber.c
@@ -0,0 +1,11 @@
+// Program: To find average of n numbers.
+// Author: Dev Mehta
+// Date: 29th July 2023.
+#include<stdio.h>
+int main(){
+    int n,average;
+    printf("Enter the number: ");
+    scanf("%d" , &n);
+    average=(n+1)/2;
+    printf("Average of n numbers is %d" , average);
+}
