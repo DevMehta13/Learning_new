70e0ddbb066c1e3f670b3d4b9a3bb2ea140a194b
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-07-29 21:09:54 +0530

    Update # 29 - sizeOfStringUsingArray.c

--- # 29 - sizeOfStringUsingArray.c
+++ # 29 - sizeOfStringUsingArray.c
@@ -13,7 +13,7 @@ int stringLength(const char *string1 ){//idhar bhi *string1 use kiya hai but nic
 int main(){
     char string1[1000];//to write statement we need to form an array and we will pass array in the function and will get its length.
     printf("Enter the statement whose length you wanna find: ");
-    scanf("%s" , string1);//its %s for string.
+    fgets(string1, sizeof(string1), stdin);//fgets is used to count length of string even including space.stdin is used to read data from the user through the command-line or terminal.fgets is used to read a line of text (including spaces) from stdin and stores it as a string. It reads until it encounters a newline character (\n) or reaches the specified maximum length.
     int length = stringLength(string1);
     printf("The size of the string is: %d" , length);
 }
