b5de4eaa261bddaaf2732bd92b31572f3d8e2004
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-07-30 18:06:08 +0530

    Update # 33 - largestNumberInAnArray.c

--- # 33 - largestNumberInAnArray.c
+++ # 33 - largestNumberInAnArray.c
@@ -1,3 +1,27 @@
 // Program: To find largest number in an array.
 // Author: Dev Mehta
 // Date: 30th July 2023.
+#include<stdio.h>
+int largestNumber(int *arr , int size){
+    int max = arr[0];
+    for(int i=1 ; i<size ; ++i){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        else{
+            max = max;
+        }
+    }
+    printf("The maximum value in the array is %d" , max);
+}
+int main(){
+    int size;
+    printf("Enter the size of an array: ");
+    scanf("%d" , &size);
+    int arr[size];
+    printf("Enter the elements of an array: ");
+    for(int i=0 ; i < size ; ++i){
+        scanf("%d" , &arr[i]);
+    }
+    largestNumber(arr,size);
+}
