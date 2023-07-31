a219362f6f0a1d85201b0bd4c9a35405b3ae1378
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2023-07-31 12:59:27 +0530

    Add files via upload

---
+++ # 40 - dataStructureArrays.c
@@ -0,0 +1,21 @@
+// Program: To create data strucutre to store data for as much students you required.
+// Author: Dev Mehta
+// Date: 31th July 2023.
+#include<stdio.h>
+struct book{
+    char name[100];
+    int class;
+    float percentage;
+};
+int main(){
+    int size,i;
+    printf("Enter the number of students whose marks you want to enter: ");
+    scanf("%d" , &size);
+    struct book b[size];
+    printf("Enter the name,class and percentage of students: \n");
+    for( i = 0 ; i < size ; i++)
+        scanf("\n %s %d %f" , b[i].name , &b[i].class , &b[i].percentage);//here we wont write &b[i].name and just write b[i].name as its string and all so aage se dhyan rakhna.
+    printf("The entered data is: \n");
+    for(int j = 0 ; j < size ; j++)
+        printf("\n%s %d %f" , b[j].name , b[j].class , b[j].percentage);
+}
